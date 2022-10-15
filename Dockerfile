ec17bd20353e9d9f6e1b8c993c31caa2b96af132
pedro-fmm <pedrofmm13@gmail.com>
2022-10-15 00:36:47 -0300

    first commit

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM python:3.10-slim
+
+EXPOSE 5000
+
+WORKDIR /api_hcm
+
+COPY . /api_hcm
+
+RUN pip install --upgrade pip --no-cache-dir -r requirements.txt
+
+CMD ["python", "src/main.py"]
---
+++ src/main.py
@@ -0,0 +1,42 @@
+
+from flask import Flask
+from flask_cors import CORS
+from flask import request
+import requests as r
+from set_envs import API_URL
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/', methods=['GET'])
+def health():
+    return {'status': 'status', 'message': 'tô vivo'}, 200
+
+@app.route('/bater-ponto', methods=['POST'])
+def baterPonto():
+
+    data = request.get_json(force=True)
+
+    try:
+        include_at = data.get('includeAt')
+        employeeId = data.get('employeeId')
+        employerId = data.get('employerId')
+    except:
+        return {'status': 422, 'response': 'Preencha todos os campos'}, 422 
+        
+    request_body = {
+        'includeAt': include_at,
+        'employeeId': employeeId,
+        'employerId': employerId
+    }
+
+    response = r.post(f'{API_URL}/proxy/ab2198a3-cafd-49d5-8ace-baac64e72222', json=request_body)
+
+    if response.status_code != 200:
+        return {'status': str(response.status_code), 'response': response.json(), 'request':request_body}, int(response.status_code)
+
+    return {'status': response.status_code, 'response': str(response.json())}, 200
+
+
+if __name__ == '__main__':
+    app.run(host="::", port=5000)
